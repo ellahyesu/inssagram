280a672e415c77081c661305a73de8d432e902f3
ellahyesu <69390318+ellahyesu@users.noreply.github.com>
2021-08-11 17:21:04 +0900

    초기 환경 세팅

---
+++ src/test/java/com/inssagram/InssagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inssagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InssagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
